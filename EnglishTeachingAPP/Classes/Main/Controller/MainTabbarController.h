671884eb93dabafa6788184b3ff7695ca78e7cfe
slowjay0117 <will@willdeMacBook-Pro.local>
2017-10-12 10:51:46 +0800

    更改项目目录
    新增了项目所需的目录

--- EnglishTeachingAPP/Classes/Main/Controller/MainTabbarController.h
+++ EnglishTeachingAPP/Classes/Main/Controller/MainTabbarController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MainTabbarController.h
 //  EnglishTeachingAPP
 //
 //  Created by will on 2017/10/12.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MainTabbarController : UITabBarController
 
 @end
-
---
+++ EnglishTeachingAPP/Classes/Main/Controller/MainTabbarController.m
@@ -0,0 +1,37 @@
+//
+//  MainTabbarController.m
+//  EnglishTeachingAPP
+//
+//  Created by will on 2017/10/12.
+//  Copyright © 2017年 will. All rights reserved.
+//
+
+#import "MainTabbarController.h"
+
+@interface MainTabbarController ()
+
+@end
+
+@implementation MainTabbarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
